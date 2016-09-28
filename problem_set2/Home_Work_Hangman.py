4573ccef58682c27cd6b189ca7eeaa1fa42f652d
ykitkevich <ykitkevich@productiveedge.com>
2016-09-28 20:45:37 +0300

    Lecture 2 Home work Hungman added

---
+++ problem_set2/Home_Work_Hangman.py
@@ -0,0 +1,69 @@
+import random
+
+max_attempt = 8
+
+
+def read_words(filename='20k.txt'):
+    ''' Read file of most popular english words'''
+    with open(filename) as words_file:
+        words = words_file.read()
+        words = [word for word in words.split('\n') if len(word) > 5]
+    return words
+
+
+def choose_random_word(words):
+    ''' Choose random word from list '''
+    return random.choice(words)
+
+
+def hangman():
+
+    word = 'qwequutq'
+    guess_word = ['_']*len(word)
+    available_letters = 'abcdefghijklmnopqrstuvwxyz'
+    pressed_letters = set()
+
+    print('Welcome to the game, Hangman! \nI am thinking of a word that is %i \n letters long.' % len(word))
+    print('-'*6)
+
+    counter = 0
+
+    while counter < max_attempt:
+        print('You have %i guesses left' % (max_attempt - counter))
+        print('Available letters:' + available_letters)
+
+        guess = input('Please guess a letter:')
+
+        if guess in word and guess in available_letters and guess != '':
+            # pressed character presents in word
+            # get char positions in word
+            indexes = [i for (i, ltr) in enumerate(word) if ltr == guess]
+
+            # put char into guessed word
+            for i in indexes:
+                guess_word[i] = guess
+            print('Good guess: %s \n' % ''.join(guess_word))
+        elif guess in pressed_letters and guess != '':
+            # pressed char not present in word or already pressed
+            print('Invalid letter, please try again. \n')
+        else:
+            # else increment attempt
+            print('Oops! That letter is not in my word: %s \n' % ''.join(guess_word))
+            counter += 1
+
+        if word == ''.join(guess_word):
+            print('You win!')
+            break
+
+        pressed_letters.add(guess)
+        available_letters = available_letters.replace(guess, '')
+
+    if word != ''.join(guess_word):
+        print('Sorry, but you lost.')
+
+    print('The word I am thinking is \'%s\'' % word)
+
+
+if __name__ == '__main__':
+
+    hangman()
---
+++ problem_set2/Home_Work_warm_up2.py
@@ -0,0 +1,30 @@
+import math
+
+vowels = ('a', 'e', 'i', 'o',  'u')
+
+
+def is_vovel(char):
+    return (char.lower() in vowels)
+
+
+def clip(lo, x, hi):
+    print(min(max(x, lo), hi))
+
+
+def polysum(n, s):
+    area = 0.25*n*s**2/math.tan(math.pi/n)
+    perimeter = n*s
+    return (area + perimeter**2)
+
+
+if __name__ == '__main__':
+
+    print("check clip function:")
+    clip(-1.62, -6.63, 1.64)
+    clip(-3.09, 3.51, 4.56)
+
+    print('check is_vovel function: ' + str(is_vovel('E')))
+    print('check is_vovel function: ' + str(is_vovel('e')))
+    print('check is_vovel function: ' + str(is_vovel('uue')))
+
+    print('check polysum: %0.4f' % polysum(22, 28))
